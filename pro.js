8dd5d083c23f638fc56ac11a2657bc7de08b4635
sathishshanmugamm <blackbkstar@gmail.com>
2024-05-23 04:16:53 +0100

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,8 @@
+let ab=document.getElementById("bik")
+let ac=document.getElementById("db")
+ab.addEventListener("mouseover",()=>{
+    ac.removeChild("")
+})
+ab.addEventListener("mouseout",()=>{
+   
+ })
